a83a747e63d6676767cd7bab6d80788c34a6a05a
j-gruenstaeudl <jakob.tobias.g@gmail.com>
2024-08-09 11:14:46 +0200

    React added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://me.gruenstaeudl.at',
-    base: '/portfolio'
+  site: 'https://me.gruenstaeudl.at',
+  base: '/portfolio',
+  integrations: [react()]
 });
